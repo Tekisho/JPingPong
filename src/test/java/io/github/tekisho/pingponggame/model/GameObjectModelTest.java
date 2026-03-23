3e5bc9baf0cc1be4884b34614ec51ec944949c49
Teki Shodo <tekishodo@gmail.com>
2026-03-23 20:32:08 +0000

    Update collision test cases in game object model test class

--- src/test/java/io/github/tekisho/pingponggame/model/GameObjectModelTest.java
+++ src/test/java/io/github/tekisho/pingponggame/model/GameObjectModelTest.java
@@ -1,5 +1,6 @@
 package io.github.tekisho.pingponggame.model;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -10,16 +11,31 @@ class GameObjectModelTest {
     final double heightBoundary = 800;
     final double widthBoundary = 600;
 
-    final GameObjectModel racketModel = new RacketModel();
+    final GameObjectModel firstRacketModel = new RacketModel();
+    final GameObjectModel secondRacketModel = new RacketModel();
     final GameObjectModel ballModel = new BallModel();
+
+    @BeforeEach
+    void resetPositionsOfGameObjects() {
+        firstRacketModel.updatePosition(0, 0);
+        secondRacketModel.updatePosition(0, 0);
+        ballModel.updatePosition(0, 0);
+    }
 
     @DisplayName("Collision detection test")
     @Test
     void collisionDetectionTest() {
-        ballModel.updatePosition(20, 0);
-        assertNull(ballModel.isCollidingWith(racketModel));
+        secondRacketModel.updatePosition(heightBoundary - secondRacketModel.getWidth(), 0);
 
-        ballModel.updatePosition(19, 0);
-        assertEquals(racketModel, ballModel.isCollidingWith(racketModel));
+        ballModel.updatePosition(firstRacketModel.getWidth(), 0);
+        assertNull(ballModel.isCollidingWith(firstRacketModel, secondRacketModel));
+
+        ballModel.updatePosition(firstRacketModel.getWidth() - 1, 0);
+        assertEquals(firstRacketModel, ballModel.isCollidingWith(firstRacketModel, secondRacketModel));
+    }
+
+    @Test
+    void resolveCollisionTest() {
+        // TODO: Implement test for resolving overlapping (stuck) collision
     }
 }
